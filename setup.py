0bb263cd87cf7939368232aa517e02802b9a529f
dumbnerd <107756166+SoappGuy@users.noreply.github.com>
2023-09-23 12:26:28 +0300

    refactored using dataclasses

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nure_tools',
-    version='1.0.1',
+    version='1.0.2',
     author='pencel_z_kavunom',
     packages=find_packages(),
     description="Simple python library to use nure-dev API",
